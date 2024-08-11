bd51ebace4b418833e2b2a83d2a2e8dc28de5598
Susanna Repo <susannar@puhti-login14.bullx>
2024-08-11 12:42:38 +0300

    Several changes to calculating and saving the results during the last few weeks
    results/explore_results_functions.R
    - Add a function to plot multiple feature importance plots in the same figure with same x-axis scale
    - Add a function that creates comparison ALE plot of the two sites given the dataset name
    - Add a function to create and save learning curve plots
    - Edit save_fi_ale_results: before saving, sort ALE results based on the feature importances and add testData to the result object
    - Set the same color scheme for all plots, edit plot labels and font sizes
    
    results/calculate_scores_fi_ale.R
    - Refactor calculate_results_from_fits, function that creates and saves score plots and calculates and saves the plots of feature importances and ALE results
    - Add function to plot all importance plots in the same figure
    - Create ALE comparison plots between two sites
    - Save learning curves from the fits
    - Edit plot labes and font sizes
    - Other changes

--- results/calculate_scores_fi_ale.R
+++ results/calculate_scores_fi_ale.R
@@ -2,46 +2,287 @@ source("/scratch/dongelr1/susannar/kesa2024/results/explore_results_functions.R"
 
 score_dir_hyy <- "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/scores"
 model_dir_hyy <- "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/fitted_models"
+lc_dir_hyy <- "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/learning_curves"
 target_dir_hyy <- "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results"
 
 score_dir_bei <- "/scratch/dongelr1/susannar/kesa2024/results/beijing/scores"
 model_dir_bei <- "/scratch/dongelr1/susannar/kesa2024/results/beijing/fitted_models"
+lc_dir_bei <- "/scratch/dongelr1/susannar/kesa2024/results/beijing/learning_curves"
 target_dir_bei <- "/scratch/dongelr1/susannar/kesa2024/results/beijing/interpret_results"
 
 
-calculate_results_from_fits <- function(site, sub_dir, title) {
-  if (site == "hyytiala") {
-    score_dir <- file.path(score_dir_hyy, sub_dir)
-    model_dir <- file.path(model_dir_hyy, sub_dir)
-    target_dir <- file.path(target_dir_hyy, sub_dir)
-  } else if (site == "beijing") {
-    score_dir <- file.path(score_dir_bei, sub_dir)
-    model_dir <- file.path(model_dir_bei, sub_dir)
-    target_dir <- file.path(target_dir_bei, sub_dir)
-  } else {
-    print(paste("Unidentified site", site))
-  }
+calculate_results_from_fits <- function(base_dir, sub_dir, title) {
+  # if (site == "hyytiala") {
+  #   score_dir <- file.path(score_dir_hyy, sub_dir)
+  #   model_dir <- file.path(model_dir_hyy, sub_dir)
+  #   target_dir <- file.path(target_dir_hyy, sub_dir)
+  # } else if (site == "beijing") {
+  #   score_dir <- file.path(score_dir_bei, sub_dir)
+  #   model_dir <- file.path(model_dir_bei, sub_dir)
+  #   target_dir <- file.path(target_dir_bei, sub_dir)
+  # } else {
+  #   print(paste("Unidentified site", site))
+  # }
+  
+  score_dir <- file.path(base_dir, "scores", sub_dir)
+  model_dir <- file.path(base_dir, "fitted_models", sub_dir)
+  target_dir <- file.path(base_dir, "interpret_results", sub_dir)
   
   create_and_save_score_plots(score_dir, target_dir, title)
   get_fi_ale_plots(model_dir, target_dir, title)
 }
 
+# Save the scores, feature importances and ALEs
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala",
+  sub_dir = "same_features_as_beijing", 
+  title = "Hyytiälä, same features as Beijing"
+)
 
-calculate_results_from_fits(site = "hyytiala", sub_dir = "same_features_as_beijing", title = "Hyytiälä, same features with Beijing")
-calculate_results_from_fits(site = "hyytiala", sub_dir = "same_features_as_beijing_no_outlier_filtering", title = "Hyytiälä, same features with Beijing (data containing outliers)")
-calculate_results_from_fits(site = "beijing", sub_dir = "same_features_as_hyy", title = "Beijing, same features with Hyytiälä")
-calculate_results_from_fits(site = "beijing", sub_dir = "same_features_as_hyy_no_outlier_filtering", title = "Beijing, same features with Hyytiälä data (data containing outliers)")
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala",
+  sub_dir = "same_features_as_beijing_no_outlier_filtering", 
+  title = "Hyytiälä, same features as Beijing (data containing outliers)"
+)
+
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/beijing",
+  sub_dir = "same_features_as_hyy", 
+  title = "Beijing, same features as Hyytiälä"
+)
+
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/beijing",
+  sub_dir = "same_features_as_hyy_no_outlier_filtering", 
+  title = "Beijing, same features as Hyytiälä data (data containing outliers)"
+)
 
 
-calculate_results_from_fits(site = "hyytiala", sub_dir = "all_feature_subsets", title =  "Hyytiälä, all feature subsets")
-calculate_results_from_fits(site = "hyytiala", sub_dir = "all_feature_subsets_no_outlier_filtering", title = "Hyytiälä, all feature subsets (data containing outliers)")
+## Scores etc. for data containing hour of the day components ##
+
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/beijing",
+  sub_dir = "same_features_as_hyy_hour", 
+  title = "Beijing, same features as Hyytiälä"
+)
+
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala",
+  sub_dir = "same_features_as_beijing_hour", 
+  title = "Hyytiälä, same features as Beijing"
+)
 
 
-# score_dfs <- list.files(path = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/scores/all_feature_subsets", full.names = TRUE) %>% lapply(readRDS)
+## Save the learning curve plots ##
+
+save_learning_curves_from_results(
+  sub_dir = "same_features_as_beijing", 
+  title = "Hyytiälä, same features as Beijing", 
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala"
+)
+
+save_learning_curves_from_results(
+  sub_dir = "same_features_as_beijing_no_outlier_filtering", 
+  title = "Hyytiälä, same features as Beijing (data containing outliers)", 
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala"
+)
+
+save_learning_curves_from_results(
+  sub_dir = "same_features_as_hyy", 
+  title = "Beijing, same features as Hyytiälä", 
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/beijing"
+)
+
+save_learning_curves_from_results(
+  sub_dir = "same_features_as_hyy_no_outlier_filtering", 
+  title = "Beijing, same features as Hyytiälä data (data containing outliers)",
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/beijing"
+)
+
+# There are no learning curves for models with hour in the train data
+
+# calculate_results_from_fits(site = "hyytiala", sub_dir = "same_features_as_beijing", title = "Hyytiälä, same features as Beijing")
+# calculate_results_from_fits(site = "hyytiala", sub_dir = "same_features_as_beijing_no_outlier_filtering", title = "Hyytiälä, same features as Beijing (data containing outliers)")
+# calculate_results_from_fits(site = "beijing", sub_dir = "same_features_as_hyy", title = "Beijing, same features as Hyytiälä")
+# calculate_results_from_fits(site = "beijing", sub_dir = "same_features_as_hyy_no_outlier_filtering", title = "Beijing, same features as Hyytiälä data (data containing outliers)")
+
+## Create score plots for the Hyytiälä models trained on all data subsets (e.g. proxies) ##
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala",
+  sub_dir = "all_feature_subsets", 
+  title = "Hyytiälä, all feature subsets"
+)
+
+calculate_results_from_fits(
+  base_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala",
+  sub_dir = "all_feature_subsets_no_outlier_filtering", 
+  title = "Hyytiälä, all feature subsets (data containing outliers)"
+)
+
+# calculate_results_from_fits(site = "hyytiala", sub_dir = "all_feature_subsets", title =  "Hyytiälä, all feature subsets")
+# calculate_results_from_fits(site = "hyytiala", sub_dir = "all_feature_subsets_no_outlier_filtering", title = "Hyytiälä, all feature subsets (data containing outliers)")
+
+###########################
+
+hyy_imp <- list.files(path = file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing"), pattern = "importances_.*.rds", full.names = TRUE) %>%
+  lapply(function(f) {
+    dataset_name <- tools::file_path_sans_ext(basename(f))
+    dataset_name <- str_remove(dataset_name, "importances_")
+    
+    df <- readRDS(f) %>% mutate(dataset_name = dataset_name)
+    return(df)
+  })
 
 
 
+###########################
+
+# Plot multiple importances in the same plot
+imp_files <- list.files(path = file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing"), pattern = "importances_.*.rds", full.names = TRUE)
+imp_files_bei <- list.files(path = file.path("/scratch/dongelr1/susannar/kesa2024/results/beijing/interpret_results/same_features_as_hyy"), pattern = "importances_.*.rds", full.names = TRUE)
+
+combine_importances_to_one_figure <- function(imp_files) {
+  plots <- imp_files %>% lapply(function(p) {
+    dataset_name <- tools::file_path_sans_ext(basename(p))
+    dataset_name <- str_remove(dataset_name, "importances_")
+    
+    # if (dataset_name == "uvb_so2_filtered") {
+    #   return(NA)
+    # }
+    
+    df <- readRDS(p)
+    # df <- df %>% mutate(dataset_name = dataset_name)
+    create_importance_plot(df, title = dataset_name)
+    # df$dataset_name <- c(dataset_name)
+  })
+  
+  g <- ggarrange(plotlist = plots, common.legend = TRUE, legend = "bottom", nrow = 1)
+  return(g)
+}
+
+g1 <- combine_importances_to_one_figure(imp_files)
+ggsave(file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing", paste0("importances_all.png")), plot = g1, width = 15, height = 6)
+
+g2 <- combine_importances_to_one_figure(imp_files_bei)
+ggsave(file.path("/scratch/dongelr1/susannar/kesa2024/results/beijing/interpret_results/same_features_as_hyy", paste0("importances_all.png")), plot = g2, width = 15, height = 6)
+
+plots <- imp_files %>% lapply(function(p) {
+  dataset_name <- tools::file_path_sans_ext(basename(p))
+  dataset_name <- str_remove(dataset_name, "importances_")
+  
+  # if (dataset_name == "uvb_so2_filtered") {
+  #   return(NA)
+  # }
+  
+  df <- readRDS(p)
+  # df <- df %>% mutate(dataset_name = dataset_name)
+  create_importance_plot(df, title = dataset_name)
+  # df$dataset_name <- c(dataset_name)
+})
+
+g <- ggarrange(plotlist = plots, common.legend = TRUE, legend = "bottom", nrow = 1)
+ggsave(file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing", paste0("importances_all.png")), plot = g, width = 15, height = 6)
+
+###########################
+
+# Not sure what's going on here
+files_hyy <- list.files(path = file.path(model_dir_hyy, "same_features_as_beijing"), pattern = "dataset.rds|dataset_uvb_so2_filtered.rds", full.names = TRUE)
+files_bei <- list.files(path = file.path(model_dir_bei, "same_features_as_hyy"), pattern = "dataset.rds|dataset_uvb_so2_filtered.rds", full.names = TRUE)
+
+fits_hyy <- lapply(files_hyy, readRDS) %>% lapply(function(x) x[[1]])
+fits_bei <- lapply(files_bei, readRDS) %>% lapply(function(x) x[[1]])
+
+plots_hyy <- fits_hyy %>% lapply(function(x) ggplot(x$rf) + ggtitle(x$datasetName))
+plots_bei <- fits_bei %>% lapply(function(x) ggplot(x$rf) + ggtitle(x$datasetName))
+
+p_hyy <- ggarrange(plotlist = plots_hyy, common.legend = TRUE, legend = "bottom")
+p_hyy <- annotate_figure(p_hyy, ggpubr::text_grob("Hyytiälä"))
+p_bei <- ggarrange(plotlist = plots_bei, common.legend = TRUE, legend = "bottom")
+p_bei <- annotate_figure(p_bei, ggpubr::text_grob("Beijing"))
+
+p_hyy
+p_bei
 
 
+# score_dfs <- list.files(path = "/scratch/dongelr1/susannar/kesa2024results/hyytiala/scores/all_feature_subsets", full.names = TRUE) %>% lapply(readRDS)
+
+get_top_ale_plots <- function(ale_path, fi_path, k = 4) {
+  ales <- readRDS(ale_path)
+  fi <- readRDS(fi_path)
+  feats <- filter(fi, model == "rf")$feature[1:k]
+  top <- ales$rf[feats]
+  test_data <- ales$rf$testData
+  
+  # p <- ggplot(dff, aes(x = .borders, y = .value, linetype = Type)) +
+  #   geom_line(linewidth = 1.3, color = "black") +
+  #   geom_line(aes(color = Type), linewidth = 1) +
+  #   labs(x = feat, y = "SA") +
+  #   scale_color_manual(values = color_palette) +
+  #   scale_linetype_manual(values = rep("solid", 4)) +
+  #   theme(panel.border=element_rect(linetype = 1, fill=NA)) +
+  #   scale_fill_manual(values = setNames(c(color_palette[5], color_palette[8]), c("rf", "lm")),
+  #                     breaks = c("rf", "lm")) +
+  #   geom_rug(data = data, aes(x = !!sym(feat), y = NULL), linetype = "solid", color = "black", alpha = 0.1)
+  # return(p)
+}
+
+ales1 <- readRDS(file.path(target_dir_hyy, "same_features_as_beijing", "ale_results_dataset.rds"))
+ales2 <- readRDS(file.path(target_dir_hyy, "same_features_as_beijing", "ale_results_dataset_uvb_so2_filtered.rds"))
+# ales2 <- readRDS(file.path(target_dir_bei, "same_features_as_hyy", "ale_dataset.png"))
+
+fi1 <- readRDS(file.path(target_dir_hyy, "same_features_as_beijing", "importances_dataset.rds"))
+fi2 <- readRDS(file.path(target_dir_hyy, "same_features_as_beijing", "importances_dataset_uvb_so2_filtered.rds"))
+
+feats <- filter(fi1, model == "rf")$feature
+View(ales1$rf[feats][1:4])
 
 
+#####################
+
+### Create feature importance plots with the same x-axis and combine them ###
+
+load_importances <- function(path, site) {
+  imp <- list.files(path = path, pattern = "importances_.*.rds", full.names = TRUE) %>% lapply(function(p) {
+    dataset_name <- tools::file_path_sans_ext(basename(p))
+    dataset_name <- str_remove(dataset_name, "importances_")
+    df <- readRDS(p)
+    df <- df %>% mutate(dataset_name = dataset_name)
+  }) %>%
+    lapply(function(x) mutate(x, site = site))
+  names(imp) = lapply(imp, function(df) unique(df$dataset_name))
+  return(imp)
+}
+
+hyy_imp <- load_importances(path = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing", site = "hyy")
+bei_imp <- load_importances(path = "/scratch/dongelr1/susannar/kesa2024/results/beijing/interpret_results/same_features_as_hyy", site = "bei")
+
+# h <- hyy_imp[c("uvb_so2_filtered", "unfiltered")]
+# b <- bei_imp[c("uvb_so2_filtered", "unfiltered")]
+h <- hyy_imp
+b <- bei_imp
+
+p1 <- create_importance_plot_from_list(h, title = "Hyytiälä")
+p2 <- create_importance_plot_from_list(b, title = "Beijing")
+
+ggsave(file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing", paste0("importances_all.png")), plot = p1, width = 15, height = 6)
+ggsave(file.path("/scratch/dongelr1/susannar/kesa2024/results/beijing/interpret_results/same_features_as_hyy", paste0("importances_all.png")), plot = p2, width = 15, height = 6)
+
+
+#####################
+
+# Compare the importances of Hyytiälä and Beijing datasets
+comb <- list(hyy_imp[["uvb_so2_filtered"]] %>% mutate(dataset_name = paste("Hyytiälä", dataset_name, sep = ", ")),
+             bei_imp[["uvb_so2_filtered"]] %>% mutate(dataset_name = paste("Beijing", dataset_name, sep = ", ")))
+create_importance_plot_from_list(comb)
+
+comb <- list(hyy_imp[["unfiltered"]] %>% mutate(dataset_name = paste("Hyytiälä", dataset_name, sep = ", ")),
+             bei_imp[["unfiltered"]] %>% mutate(dataset_name = paste("Beijing", dataset_name, sep = ", ")))
+create_importance_plot_from_list(comb)
+
+### Create ALE comparison plots ###
+
+p1 <- create_ale_comparison_plots("unfiltered", hyy_fi = hyy_imp, bei_fi = bei_imp, n = 25)
+p2 <- create_ale_comparison_plots("uvb_so2_filtered", hyy_fi = hyy_imp, bei_fi = bei_imp, n = 25)
+
+plot(p1)
+plot(p2)
--- results/explore_results_functions.R
+++ results/explore_results_functions.R
@@ -35,7 +35,10 @@ target_dir <- "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/original_mod
 
 # color_palette <- brewer.pal(n = 4, name = "Set1")
 
-color_palette <- brewer.pal(n = 8, name = "Set2")[2:3]
+# color_palette <- brewer.pal(n = 8, name = "Set2")[2:3]
+color_palette <- brewer.pal(n = 8, name = "Set2")[7:8]
+# color_palette <- brewer.pal(n = 8, name = "Pastel1")[8:9]
+# color_palette <- brewer.pal(n = 8, name = "Pastel2")[7:8]
 
 # plot_scores <- function(data, metric, title = "") {
 plot_scores <- function(data, metric) {
@@ -78,7 +81,7 @@ plot_scores <- function(data, metric) {
     theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 12)) +
     # theme(axis.text.y = element_text(size = 13)) +
     # theme(axis.text.y.right = element_blank(), axis.ticks.y.right = element_blank()) +
-    geom_text(aes(label = sprintf("%.2f", score)), position = position_dodge(width = 0.9), vjust = -0.3)
+    geom_text(aes(label = sprintf("%.2f", score)), position = position_dodge(width = 0.9), vjust = -0.3, size = 3)
   
   return(p + theme(legend.position = "bottom"))
 }
@@ -108,6 +111,7 @@ create_and_save_score_plots <- function(score_dir, target_dir, main_title = "") 
 
 #######
 
+# score_dir_hyy <- "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/scores/same_features_as_beijing"
 # create_score_plots(score_dir_hyy, "Hyytiälä")
 # create_score_plots(score_dir_bei, "Beijing")
 
@@ -174,6 +178,10 @@ create_importance_plot <- function(combined_importance, title = "") {
     unique(c(1.0, pretty(x)))
   }
   
+  # Spell out the model names to match the color labels in scale_fill_manual below
+  combined_importance <- combined_importance %>%
+    mutate(model = dplyr::recode(model, rf = "random forest", lm = "linear model"))
+  
   p <- ggplot(combined_importance, aes(x = importance, y = feature, shape = model)) +
     geom_errorbar(aes(xmin = importance.05, xmax = importance.95), width = 0, linewidth = 1,
                   position = position_dodge(width = 0.5)) + 
@@ -181,6 +189,7 @@ create_importance_plot <- function(combined_importance, title = "") {
                position = position_dodge(width = 0.5)) +
     # scale_y_discrete(expand = expansion(mult = c(0.01, 0.01))) +
     scale_x_continuous(breaks = custom_breaks) +
+    # scale_fill_manual(values = setNames(color_palette, c("rf", "lm"))) +
     scale_fill_manual(values = setNames(color_palette, c("random forest", "linear model"))) +
     scale_shape_manual(values = c(21, 21)) +
     theme(panel.border = element_rect(linetype = 1, fill = NA)) +
@@ -194,6 +203,32 @@ create_importance_plot <- function(combined_importance, title = "") {
   
   p <- p + geom_hline(yintercept = seq(1.5, length(unique(combined_importance$feature)) - 0.5, by = 1), 
                       linetype = "dashed", color = "grey")
+}
+
+# Create importance plot from list of feature importance dfs with shared x-axis
+create_importance_plot_from_list <- function(importance_list, title = "") {
+  
+  x_max <- max(sapply(importance_list, function(x) max(x$importance.95)))
+  x_min <- min(sapply(importance_list, function(x) min(x$importance.05)))
+  x_max <- ceiling(x_max * 10) / 10
+  x_min <- floor(x_min * 10) / 10
+  xlimits <- c(x_min, x_max)
+  
+  plots <- lapply(importance_list, function(df) {
+    create_importance_plot(df, title = df$dataset_name)
+  })
+  
+  custom_breaks <- function(x) {
+    unique(c(1.0, pretty(x)))
+  }
+  
+  plots <- lapply(plots, function(p) {
+    p + scale_x_continuous(breaks = custom_breaks, limits = xlimits)
+  })
+  
+  g <- ggarrange(plotlist = plots, common.legend = TRUE, legend = "bottom", nrow = 1)
+  g <- annotate_figure(g, ggpubr::text_grob(title))
+  return(g)
 }
 
 plot_feature_effect_models <- function(feat, data, res, bgc) {
@@ -272,15 +307,23 @@ save_fi_ale_results <- function(path, target_dir, title = "") {
   
   # p_0_imp <- create_importance_plot(combined_importance, title = paste("Unfiltered SA data,", fit_obj$datasetName))
   fi_plot <- create_importance_plot(combined_importance, title = title)
-  ggsave(file.path(target_dir, paste0("importances_", dataset_name, ".png")), plot = fi_plot, width = 9, height = 6)
+  ggsave(file.path(target_dir, paste0("importances_", dataset_name, ".png")), plot = fi_plot, width = 5, height = 6)
   
+  # Calculate ALE results for both of the models
   eff_rf <- get_ale_results(pred_rf)
   eff_lm <- get_ale_results(pred_lm)
   
   print("ALE results calculated")
   
+  # Sort ALE results as the according to the importances of features
+  feat_rf <- combined_importance %>% filter(model == "rf") %>% .$feature
+  feat_lm <- combined_importance %>% filter(model == "lm") %>% .$feature
+  eff_rf <- eff_rf[feat_rf]
+  eff_lm <- eff_lm[feat_lm]
+  
   ale_results <- list(eff_rf, eff_lm)
   names(ale_results) <- list("rf", "lm")
+  ale_results$testData <- fit_obj$testData
   target <- file.path(target_dir, paste0("ale_results_", dataset_name, ".rds"))
   save_data(ale_results, target)
   
@@ -303,6 +346,95 @@ get_fi_ale_plots <- function(model_dir, target_dir, title = "") {
   plots <- purrr::map(model_paths, save_fi_ale_results, target_dir = target_dir, title)
   return(plots)
 }
+
+# For testing/debugging
+# save_fi_ale_results(path = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/fitted_models/same_features_as_beijing/dataset_uvb_so2_filtered.rds",
+#                     target_dir = "/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing",
+#                     title = "Hyytiälä, same features as Beijing")
+
+
+### Functions to plot ALE comparison plots
+plot_comparisons <- function(plotlist) {
+  l <- lapply(plotlist, function(sublist) { g <- ggarrange(plotlist = sublist, common.legend = TRUE, legend = "bottom"); })
+  splitted_l <- split(l, ceiling(seq_along(l) / 2))
+  plots <- lapply(splitted_l, function(sublist) { g <- ggarrange(plotlist = sublist, common.legend = TRUE, legend = "bottom", ncol = 1, nrow = 2); })
+  return(plots)
+}
+
+create_ale_comparison_plots <- function(dataset_name, hyy_fi, bei_fi, n) {
+  h_path <- file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/interpret_results/same_features_as_beijing")
+  b_path <- file.path("/scratch/dongelr1/susannar/kesa2024/results/beijing/interpret_results/same_features_as_hyy")
+  
+  ale_path_hyy <- file.path(h_path, paste0("ale_results_", dataset_name, ".rds"))
+  ale_path_bei <- file.path(b_path, paste0("ale_results_", dataset_name, ".rds"))
+  ale_hyy <- readRDS(ale_path_hyy)
+  ale_bei <- readRDS(ale_path_bei)
+  
+  fit_hyy <- readRDS(file.path("/scratch/dongelr1/susannar/kesa2024/results/hyytiala/fitted_models/same_features_as_beijing", paste0(dataset_name, ".rds")))[[1]]
+  fit_bei <- readRDS(file.path("/scratch/dongelr1/susannar/kesa2024/results/beijing/fitted_models/same_features_as_hyy", paste0(dataset_name, ".rds")))[[1]]
+
+  h_fi <- hyy_fi[[dataset_name]]
+  b_fi <- bei_fi[[dataset_name]]
+  
+  comb_fi <- rbind(h_fi, b_fi)
+  av_fi <- average_importances(comb_fi)
+  features <- arrange(av_fi, desc(mean_importance))$feature
+  
+  if (n <= length(features)) {
+    features <- features[1:n]
+  }
+  
+  fplots <- list()
+  for (k in 1:length(features)) {
+    ph <- plot_feature_effect_models(features[[k]], fit_hyy$testData, ale_hyy) + ggtitle("Hyytiälä")
+    pb <- plot_feature_effect_models(features[[k]], fit_bei$testData, ale_bei) + ggtitle("Beijing")
+    plots <- list(ph, pb)
+    # names(plots) <- c(paste(m[[i]], "hyy"), paste(m[[i]], "bei"))
+    names(plots) <- c("unfiltered", "unfiltered")
+    fplots[[k]] <- plots
+  }
+  
+  plot_test <- plot_comparisons(fplots)
+  g <- ggarrange(plotlist = plot_test, common.legend = TRUE, legend = "bottom")
+  g <- annotate_figure(g, ggpubr::text_grob(dataset_name))
+  return(g)
+}
+
+########## Plot and save learning curves ##############
+
+save_learning_curves_from_results <- function(sub_dir, title, base_dir) {
+  lc_dir <- file.path(base_dir, "learning_curves", sub_dir)
+  lc_dfs <- list.files(path = lc_dir, full.names = TRUE) %>% lapply(readRDS)
+  print(paste("Learning curves loaded from", lc_dir))
+  
+  # Exclude the resampling data and write out the method names
+  lc_dfs <- lc_dfs %>%
+    lapply(function(x) filter(x, Data != "Resampling")) %>%
+    lapply(function(x) mutate(x, method = dplyr::recode(method, rf = "random forest", lm = "linear model")))
+  
+  plots_all <- lc_dfs %>% lapply(function(lc) {
+    ggplot(lc, aes(x = Training_Size, y = RMSE, color = Data)) +
+      # geom_smooth(method = loess, span = .8, se = FALSE) +
+      # geom_smooth(method = loess, span = .8, formula = y ~ x) +
+      geom_line() +
+      geom_point(shape = 1) +
+      facet_wrap(~method) +
+      scale_y_continuous(labels = function(x) format(x, scientific = TRUE)) +
+      theme_bw() +
+      ggtitle(lc$dataset_name)
+  })
+  
+  g <- ggarrange(plotlist = plots_all, common.legend = TRUE, legend = "bottom")
+  g <- annotate_figure(g, ggpubr::text_grob(title))
+  
+  target_dir <- file.path(base_dir, "interpret_results", sub_dir)
+  ggsave(file.path(target_dir, paste0("lc_all.png")), plot = g, width = 9, height = 6)
+  print(paste("Learning curve saved to", target_dir))
+}
+
+
+
+
 
 ########
 
